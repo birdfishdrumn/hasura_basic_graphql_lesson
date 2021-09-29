af81b0b3f7f95395b7be42fbfb9a4563f001b31f
michihiro <pomeofishbird0303@gmail.com>
2021-09-29 19:03:31 +0900

    graphqlの方を作成

--- queries/queries.ts
+++ queries/queries.ts
@@ -29,7 +29,7 @@ query GetUsersIds {
 `
 // 特定のidをもったuser情報を取得する。
 export const GET_USERBY_ID = gql`
-query GetUserById($id: uuid) {
+query GetUserById($id: uuid!) {
   users_by_pk(id: $id){
     id
     name
@@ -37,3 +37,34 @@ query GetUserById($id: uuid) {
   }
 }
 `
+//String!の!は引数が必須であるという意味
+export const CREATE_USER = gql`
+ mutation CreateUser($name: String!){
+    insert_users_one(object: {name: $name}) {
+    id
+    name
+    created_at
+  }
+ }
+`
+//Userの削除
+export const DELETE_USER = gql`
+ mutation DeleteUser($id: uuid!){
+     delete_users_by_pk(id: $id) {
+    id
+    name
+    created_at
+  }
+ }
+ `
+
+//User情報の更新
+export const UPDATE_USER = gql`
+ mutation UpdateUser($id: uuid!,$name: String!){
+    update_users_by_pk(pk_columns: {id: $id},_set: {name:$name}) {
+    id
+    name
+    created_at
+  }
+ }
+ `
