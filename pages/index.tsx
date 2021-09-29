eaa8e52b2f2ff349eace81c3481736583bdb872a
michihiro <pomeofishbird0303@gmail.com>
2021-09-29 21:28:35 +0900

    layoutの作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Layout } from "../components/Layout";
 
 
 export default function Home() {
   return (
-    <div>
+    <Layout title="Home" >
      <h1 className="text-red-500">Next.js!</h1>
-    </div>
+    </Layout>
   )
 }
